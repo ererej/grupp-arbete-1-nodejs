9ab82539481a52fdbcf35160005c7ec54d9b280e
Ererej <erikeliassonjohansson@gmail.com>
2024-04-10 16:01:30 +0200

    fixed buttons

--- index.js
+++ index.js
@@ -36,16 +36,6 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const background = document.getElementById("background")
 
-// function drawbuttons(){
-//     ctx.fillStyle = "#99B080"
-//     ctx.fillRect(1100,300,140,100)
-//     ctx.fillRect(200,300,140,100)  
-
-//     ctx.fillStyle = "#FAF8ED"
-//     ctx.font = "48px serif"
-//     ctx.fillText("HIT", 230,366,100)
-//     ctx.fillText("STAND", 1120,366,100)
-// }  
 
 
 
@@ -61,17 +51,66 @@ const shuffelCards = () => {
 }
 
 
-let buttons = []
-class Buttons {
-    constructor(name, x, y, width, heighet) {
-        this.name = name
+
+class Button {
+    constructor(name, x, y, width, height) {
+        this.name = name,
+        this.id = name,
+        this.x = x,
+        this.y = y,
+        this.width = width,
+        this.height = height
     }
 }
+let buttons = []
+buttons.push(new Button("hit", 200, 300, 140, 100))
+buttons.push(new Button("stand", 900, 300, 140, 100))
+
+const drawbuttons = () => {
+    buttons.forEach(button => {
+        ctx.beginPath()
+        ctx.rect(button.x, button.y, button.width, button.height)
+        ctx.fillStyle = 'rgba(225,225,225,0.5)'
+        ctx.fill()
+        ctx.lineWidth = 2
+        ctx.strokeStyle = '#000000'
+        ctx.stroke()
+        ctx.closePath()
+        ctx.font = '40px serif'
+        ctx.fillStyle = 'green'
+        ctx.fillText(button.name, button.x + button.width / 7, button.y + 64)
+    })
+}
+
+const mousePos = (canvas, event) => {
+    var boundingBox = canvas.getBoundingClientRect();
+    return {
+      x: event.clientX - boundingBox.left,
+      y: event.clientY - boundingBox.top,
+    };
+  }
+
+function tuching(pos, button) {
+return pos.x > button.x && pos.x < button.x + button.width && pos.y < button.y + button.height && pos.y > button.y
+}
+
+canvas.addEventListener('click', function(event) {
+    var mousePosition = mousePos(canvas, event);
+    let i = 0;
+    while (i < buttons.length) {
+        const button = buttons[i];
+        if (tuching(mousePosition, button)) {
+            alert(`clicked ${button.name}`);
+        }
+        i++;
+    }
+}, false)
 
 
 function draw() {
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
     drawbuttons()
+
     requestAnimationFrame(draw);
 };
 requestAnimationFrame(draw);
