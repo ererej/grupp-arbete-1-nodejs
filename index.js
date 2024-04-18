f917c1d321548d70b99a3e2a1bd59ea445808fd2
Ererej <erikeliassonjohansson@gmail.com>
2024-04-18 14:02:54 +0200

    started work on saving the users cash

--- index.js
+++ index.js
@@ -479,8 +479,37 @@ function restart(){
         console.log("restocked")
     }
 }
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+let cash = 0
+if (getCookie("cash") == "") {
+    setCookie("cash", "1000", 365)
+} else {
+    cash = getCookie("cash")
+}
+
 let discardPile = []
-let cash = 1000
 let bet = 0
 let bets = []
 restockCards()
